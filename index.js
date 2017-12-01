fd890bee8d0fe476005a5a8ef4ae6db7f51e59b3
Charlie Ciccia <cciccia@gmail.com>
2017-12-01 13:06:27 -0600

    day 1 solution

---
+++ index.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+
+rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question('Paste your puzzle input here: ', data => {
+    compute(data);
+    rl.close();
+});
+
+
+function compute(data) {
+    const result = [...data].reduce((prev, c, i) => {
+        const cur = parseInt(c)
+        const next = parseInt(data[(i+1) % data.length]);
+        return !isNaN(cur) && !isNaN(next) &&  (cur === next) ? prev + cur : prev;
+    }, 0);
+
+    console.log(result);
+}
