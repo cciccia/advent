662d28c36a4adf230f0ce510950f98f6c71b7fc0
Charlie Ciccia <charlie.ciccia@singlewire.com>
2017-12-05 10:41:34 -0600

    day 5

---
+++ day-5/index2.js
@@ -0,0 +1,21 @@
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+
+fs.readFileAsync(process.argv[2], "utf8")
+    .then(data => {
+        const instructions = data.split('\n');
+        const upperBound = instructions.length;
+        let count = 0;
+        let pointer = 0;
+
+        while (true) {
+            const offset = instructions[pointer];
+            pointer += instructions[pointer] >= 3 ? instructions[pointer]-- : instructions[pointer]++;
+            count++;
+
+            if (pointer < 0 || pointer >= upperBound) {
+                break;
+            }
+        }
+        console.log(count);
+    });
