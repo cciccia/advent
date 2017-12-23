fed574a61c8337fdd0ecd932425ab031535ba65b
Charlie Ciccia <charlie.ciccia@singlewire.com>
2017-12-23 00:30:52 -0600

    explanation

--- day-23/index2.js
+++ day-23/index2.js
@@ -7,5 +7,6 @@ fs.readFileAsync(process.argv[2], "utf8")
     });
 
 function lol(primes) {
+    // this basically says 'how many numbers between (105700 and 122000, inclusive, stepping by 17) aren't prime', which is what that program is (slowly) doing
     return 1001 - (primes.filter(prime => !((prime - 105700) % 17)).length);
 }
