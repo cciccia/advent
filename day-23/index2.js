c17dcb44374bcefd970802c3a1ada34696731909
Charlie Ciccia <charlie.ciccia@singlewire.com>
2017-12-23 00:26:58 -0600

    lol

--- day-23/index2.js
+++ day-23/index2.js
@@ -3,16 +3,9 @@ const fs = Promise.promisifyAll(require('fs'));
 
 fs.readFileAsync(process.argv[2], "utf8")
     .then(data => {
-        console.log(f(new Set(data.replace(/\n/g, ' ').split(' ').map(num => parseInt(num)))));
+        console.log(lol(data.replace(/\n/g, ' ').split(' ').map(num => parseInt(num))));
     });
 
-function f(primes) {
-    let answer = 0;
-    for (i = 105700; i <= 122700; i += 17) {
-    
-        if (!primes.has(i)) {
-            answer++;
-        }
-    }
-    return answer;
+function lol(primes) {
+    return 1001 - (primes.filter(prime => !((prime - 105700) % 17)).length);
 }
