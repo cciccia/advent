38d3bd5adf888dff33e3b4cff8e17abaf8554056
Charlie Ciccia <charlie.ciccia@singlewire.com>
2017-12-28 14:14:49 -0600

    a little cleanup

--- day-23/index2.js
+++ day-23/index2.js
@@ -3,10 +3,11 @@ const fs = Promise.promisifyAll(require('fs'));
 
 fs.readFileAsync(process.argv[2], "utf8")
     .then(data => {
-        console.log(lol(data.replace(/\n/g, ' ').split(' ').map(num => parseInt(num))));
+        console.log(lol(data.split('\n').map(num => parseInt(num))));
     });
 
 function lol(primes) {
     // this basically says 'how many numbers between (105700 and 122000, inclusive, stepping by 17) aren't prime', which is what that program is (slowly) doing
+    // Since lists of primes are easy to look up online, I just pasted the range I wanted to look at into a text file
     return 1001 - (primes.filter(prime => !((prime - 105700) % 17)).length);
 }
